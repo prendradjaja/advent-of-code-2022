bc0a3401cde6b47cfea83791272278624d33ded9
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-12-01 23:22:13 -0600

    02

---
+++ 02/a.js
@@ -0,0 +1,46 @@
+const { readFileSync } = require('fs');
+
+const interpret = {
+  'A': 'R',
+  'B': 'P',
+  'C': 'S',
+  'X': 'R',
+  'Y': 'P',
+  'Z': 'S',
+};
+
+function main() {
+  const answer = readFileSync('in', 'utf8')
+    .trimEnd()
+    .split('\n')
+    .map(
+      line => {
+        let [l, r] = line.split(' ');
+        l = interpret[l];
+        r = interpret[r];
+        const choicePoints = {'R': 1, 'P': 2, 'S': 3}[r];
+        const resultPoints = play(l, r);
+        return choicePoints + resultPoints;
+      }
+    )
+    .reduce((a, b) => a + b)
+
+  console.log(answer);
+}
+
+function succ(x) {
+  const RPS = 'RPS';
+  return RPS[(RPS.indexOf(x) + 1) % 3];
+}
+
+function play(l, r) {
+  if (l === r) {
+    return 3;
+  } else if (succ(l) === r) {
+    return 6;
+  } else {
+    return 0;
+  }
+}
+
+main();
