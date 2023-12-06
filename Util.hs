49f43589d9ba05cf1b4afec5ea78adf260036604
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2023-12-06 14:47:02 -0600

    Create Util.hs

---
+++ Util.hs
@@ -0,0 +1,25 @@
+module Util where
+
+import Data.List (isPrefixOf)
+
+
+readInt = read :: String -> Int
+
+
+split sep s = split' sep s [] ""
+  where
+    split' sep s parts curr
+      | null s =
+          parts ++ [curr]
+      | sep `isPrefixOf` s =
+          split'
+            sep
+            (drop (length sep) s)
+            (parts ++ [curr])
+            ""
+      | otherwise =
+          split'
+            sep
+            (tail s)
+            parts
+            (curr ++ [(head s)])
